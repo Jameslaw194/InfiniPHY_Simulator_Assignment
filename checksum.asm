91042dffa32e3af1af4818a828aece7d5f545c99
James <138827786+Jameslaw194@users.noreply.github.com>
2026-06-15 23:01:01 +0100

    Add exec_adh and use adh in checksum.asm
    Implemented new 'adh' instruction in CPU (exec_adh) which adds both 16-bit half-words of the source register to the destination (rd += rs[31:16] + rs[15:0]) and updates the zero flag.
    Replaced the previous manual half-word extraction/sum sequences in checksum.asm with the new adh instruction to simplify and clarify the checksum loop.

--- checksum.asm
+++ checksum.asm
@@ -6,26 +6,18 @@ add r1 r0 0
 add r2 r0 0
 add r6 r0 5
 ld r3 r2 0
-slr r4 r3 16 0
-sub r5 r5
-sll r5 r3 0 12
-add r1 r4 0
-add r1 r5 0
+adh r1 r3
 add r2 r0 4
 sub r6 r0 1
-jnz pc -36
-slr r4 r1 16 0
-sub r5 r5
-sll r5 r1 0 12
+jnz pc -20
+sub r4 r4
+add r4 r1 0
 sub r1 r1
-add r1 r4 0
-add r1 r5 0
-slr r4 r1 16 0
-sub r5 r5
-sll r5 r1 0 12
+adh r1 r4
+sub r4 r4
+add r4 r1 0
 sub r1 r1
-add r1 r4 0
-add r1 r5 0
+adh r1 r4
 neg r1 r1
 ld r3 r0 8
 sll r3 r1 0 12
